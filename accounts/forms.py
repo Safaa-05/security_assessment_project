6e4e419cd9d6001f7002be3e826bfd20fc99fed5
Safaa-05 <safaavlr@gmail.com>
2026-07-12 03:11:44 +0530

    Improve registration and login form styling

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,7 +18,51 @@ class RegisterForm(UserCreationForm):
             "password2",
         ]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.update({
+            "class": "form-control form-control-lg",
+            "placeholder": "Enter your username",
+        })
+
+        self.fields["email"].widget.attrs.update({
+            "class": "form-control form-control-lg",
+            "placeholder": "Enter your email",
+        })
+
+        self.fields["phone_number"].widget.attrs.update({
+            "class": "form-control form-control-lg",
+            "placeholder": "Enter your phone number",
+        })
+
+        self.fields["password1"].widget.attrs.update({
+            "class": "form-control form-control-lg",
+            "placeholder": "Enter your password",
+        })
+
+        self.fields["password2"].widget.attrs.update({
+            "class": "form-control form-control-lg",
+            "placeholder": "Confirm your password",
+        })
+
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter your username",
+            }
+        ),
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter your password",
+            }
+        ),
+    )
